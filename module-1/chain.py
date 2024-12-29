b8239f3e8272e9f664a4be00edbb9bc816f2341f
EmiRoberti77 <emi@datamotion.ai>
2024-12-29 12:31:55 +0000

    add AIMessage and HunmanMessage to LLM to response

--- module-1/chain.py
+++ module-1/chain.py
@@ -1,5 +1,13 @@
+import os
+import json
 from pprint import pprint
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage
+
+
+load_dotenv()
+api_key = os.getenv('OPENAI_API_KEY')
 
 messages = [AIMessage(content="You said you are doing research", name="Model")]
 messages.append(HumanMessage(content="yes thats right", name="Emi"))
@@ -8,3 +16,8 @@ messages.append(HumanMessage(content="I want to learn how AI works", name="Emi")
 
 for m in messages:
     m.pretty_print()
+
+llm = ChatOpenAI(model='gpt-4o', openai_api_key=api_key)
+result = llm.invoke(messages)
+print(type(result))
+result.pretty_print()
