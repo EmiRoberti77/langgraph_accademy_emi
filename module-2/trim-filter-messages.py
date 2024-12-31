a63f74e66fb8a47f4af980c1a3da74124453fb83
EmiRoberti77 <emi@datamotion.ai>
2024-12-31 13:53:42 +0000

    added RemoveMessage to filter messages in the graph ( save tokens )

--- module-2/trim-filter-messages.py
+++ module-2/trim-filter-messages.py
@@ -1,18 +1,36 @@
 from pprint import pprint
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import RemoveMessage
+from langgraph.graph import StateGraph, START, END, MessagesState
 from langchain_core.messages import AIMessage, HumanMessage
 from dotenv import load_dotenv
 import os
 import json
+load_dotenv()
+# get openai api_key
+api_key:str = os.getenv("OPENAI_API_KEY")
+#create message[] 
 messages = [AIMessage(content="so you like cars")]
 messages.append(HumanMessage(content="my favourite cars are ferraris"))
-load_dotenv()
-api_key:str = os.getenv("OPENAI_API_KEY")
-
-for m in messages:
-    m.pretty_print()
-
-model = ChatOpenAI(model="gpt-4o", openai_api_key=api_key)
-response = model.invoke(messages)
-
-print(response.content)
+# create instance of the llm model
+model = ChatOpenAI(model="gpt-4", openai_api_key=api_key)
+def filterMessages(state: MessagesState):
+    # Access messages directly from the state
+    filtered_messages = [RemoveMessage(id=m.id) for m in state["messages"][:-1]]
+    return {"messages": filtered_messages}
+# invole model method to use the in the graph
+def invokeModel(state:MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages":state["messages"] + [response]}
+#build the graph
+builder = StateGraph(MessagesState)
+builder.add_node("filtered_messages", filterMessages)
+builder.add_node("invoke_model", invokeModel)
+builder.add_edge(START, "filtered_messages")
+builder.add_edge("filtered_messages", "invoke_model")
+builder.add_edge("invoke_model", END)
+graph = builder.compile()
+output = graph.invoke({"messages": messages})
+for message in output["messages"]:
+    print(f"Role: {message.type}")
+    print(f"Content: {message.content}\n")
